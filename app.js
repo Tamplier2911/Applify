4e1196acb72b17fb6dc665c615356fec59fc4655
Tamplier2911 <44438728+Tamplier2911@users.noreply.github.com>
2020-05-12 15:58:45 +0300

    ..

--- app.js
+++ app.js
@@ -75,22 +75,22 @@ app.use(helmet());
 app.use(
   csp({
     directives: {
-      defaultSrc: [
-        "'self'",
-        "applify-tech.com",
-        "applify-s.herokuapp.com",
-        "fonts.googleapis.com",
-      ],
-      scriptSrc: [
-        "'self'",
-        "'unsafe-inline'",
-        "'unsafe-eval'",
-        "'sha256-GDUfxAiaE5lVgQ6z2nbmzD2HvviBuHsUsSnoHzDvfdo='",
-        "storage.googleapis.com",
-        "applify-tech.com",
-        "applify-s.herokuapp.com",
-      ],
-      fontSrc: ["'self'", "fonts.googleapis.com", "fonts.gstatic.com"],
+      // defaultSrc: [
+      //   "'self'",
+      //   "applify-tech.com",
+      //   "applify-s.herokuapp.com",
+      //   "fonts.googleapis.com",
+      // ],
+      // scriptSrc: [
+      //   "'self'",
+      //   "'unsafe-inline'",
+      //   "'unsafe-eval'",
+      //   "'sha256-GDUfxAiaE5lVgQ6z2nbmzD2HvviBuHsUsSnoHzDvfdo='",
+      //   "storage.googleapis.com",
+      //   "applify-tech.com",
+      //   "applify-s.herokuapp.com",
+      // ],
+      // fontSrc: ["'self'", "fonts.googleapis.com", "fonts.gstatic.com"],
       objectSrc: ["'self'"],
     },
   })
